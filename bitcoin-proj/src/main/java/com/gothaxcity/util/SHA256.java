db822e87818de4d2ab31da0984aea70627804b57
Gothax <scott990328@gmail.com>
2024-11-28 01:05:49 +0900

    feat: encoding hex->base64 modify
    - hash된 byte code를 base64로 인코딩하는 것으로 통일

--- bitcoin-proj/src/main/java/com/gothaxcity/util/SHA256.java
+++ bitcoin-proj/src/main/java/com/gothaxcity/util/SHA256.java
@@ -3,23 +3,45 @@ package com.gothaxcity.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import static java.util.Base64.*;
+
 public class SHA256 {
 
-    public static String encrypt(String input) {
+    public static String encryptGetEncode(String input) {
         try {
-            MessageDigest instance = MessageDigest.getInstance("SHA-256");
-            instance.update(input.getBytes());
-            byte[] digest = instance.digest();
-
-            StringBuilder builder = new StringBuilder();
-            for (byte b : digest) {
-                builder.append(String.format("%02x", b));
-            }
-            return builder.toString();
+            byte[] digest = encrypt(input);
+            return getBase64(digest);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
+    }
+
+    public static byte[] encryptGetBytes(String input) {
+        try {
+            return encrypt(input);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static byte[] encrypt(String input) throws NoSuchAlgorithmException {
+        MessageDigest instance = MessageDigest.getInstance("SHA-256");
+        instance.update(input.getBytes());
+        return instance.digest();
+    }
+
+    private static String getHex(byte[] digest) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : digest) {
+            builder.append(String.format("%02x", b));
+        }
+        return builder.toString();
+    }
+
+    private static String getBase64(byte[] digest) {
+        return getEncoder().encodeToString(digest);
     }
 }
 
