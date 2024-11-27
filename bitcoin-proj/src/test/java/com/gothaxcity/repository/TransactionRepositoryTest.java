e8b102018cc60ea818d45554a4f44c364b33c079
Gothax <scott990328@gmail.com>
2024-11-28 01:10:36 +0900

    test: test add

--- bitcoin-proj/src/test/java/com/gothaxcity/repository/TransactionRepositoryTest.java
+++ bitcoin-proj/src/test/java/com/gothaxcity/repository/TransactionRepositoryTest.java
@@ -14,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionRepositoryTest {
 //
-//    private final UtxoRepository utxoRepository = new UtxoRepository();
-//    private final TransactionRepository transactionRepository = new TransactionRepository();
+    private final UtxoRepository utxoRepository = new UtxoRepository();
+    private final TransactionRepository transactionRepository = new TransactionRepository();
 
     TransactionRepositoryTest() throws IOException {
     }
@@ -82,15 +82,33 @@ class TransactionRepositoryTest {
     void getTransactions() throws IOException {
         // given
 
-        UtxoRepository utxoRepository = new UtxoRepository();
+//        UtxoRepository utxoRepository = new UtxoRepository();
 
-        TransactionRepository transactionRepository = new TransactionRepository();
+//        TransactionRepository transactionRepository = new TransactionRepository();
         List<Transaction> transactions = transactionRepository.getTransactions();
         System.out.println("transactions = " + transactions);
         System.out.println("transactions.getFirst().toHashTxt() = \n" + transactions.getFirst().toHashTxt());
         // when
         // then
         assertNotNull(transactions);
+
+    }
+
+    @Test
+    @DisplayName("해시값으로 tx id를 만들기 위한 to string 메서드 테스트")
+    void testToHashTxt() throws IOException {
+        // given
+        // when
+        List<Transaction> transactions = transactionRepository.getTransactions();
+        System.out.println(transactions.getFirst().toHashTxt());
+        // then
+//        org.assertj.core.api.Assertions.assertThat()
+
+
+        String expected = "input: tx1, 0, 100000, DUP HASH 7c39be612c03d1a6ba4775fb3dcc07aa56da2db5 EQUALVERIFY CHECKSIG, 3045022100c12a7d54972f26d14cb311339b5122f8c187417dde1e8efb6841f55c34220ae0022066632c5cd4161efa3a2837764eee9eb84975dd54c2de2865e9752585c53e7cce\n" +
+                "output: 0, 40000, DUP HASH 9d8e7f6a5b4c3d2e1f0a9b8c7d6e5f4a3b2c1d0 EQUALVERIFY CHECKSIG\n" +
+                "output: 1, 59000, DUP HASH 1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s EQUALVERIFY CHECKSIG\n";
+        assertEquals(expected, transactions.getFirst().toHashTxt());
 
     }
 
