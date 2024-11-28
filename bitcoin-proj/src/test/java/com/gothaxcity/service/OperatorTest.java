3b787246923f1a6242e240b7f54b811e28d87e93
Gothax <scott990328@gmail.com>
2024-11-29 01:15:32 +0900

    test: multiSignature, 분기문 처리 로직 추가
    - multi sig 통과
    - coverage 100%

--- bitcoin-proj/src/test/java/com/gothaxcity/service/OperatorTest.java
+++ bitcoin-proj/src/test/java/com/gothaxcity/service/OperatorTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.security.KeyPair;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OperatorTest {
 
@@ -14,7 +19,7 @@ class OperatorTest {
     void pushScriptToStack() {
         // given
 
-        String lockingScript = "DUP HASH <pubKeyHash> EQUALVERIFY CHECKSIG";
+        String lockingScript = "OP_DUP OP_HASH <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG OP_CHECKFINALRESULT";
         String unlockingScript = "<sig> <pubKey>";
         Operator operator = new Operator(lockingScript, unlockingScript, "message");
         // when
@@ -25,8 +30,8 @@ class OperatorTest {
 
 
     @Test
-    @DisplayName("P2PKH SCRIPT test 성공 사례")
-    void testP2PKH() {
+    @DisplayName("P2PKH SCRIPT test 성공 사례 - 테스트 메시지")
+    void testP2PKHWithTestMessage() {
         // given
         KeyPair keyPair = ECDSA.generateRandomKeyPair();
         String message = "Hello this is test message";
@@ -37,7 +42,7 @@ class OperatorTest {
 
 //        String lockingScript = "DUP HASH <pubKeyHash> EQUALVERIFY CHECKSIG";
 //        String unlockingScript = "<sig> <pubKey>";
-        String lockingScript = "DUP HASH " + SHA256.encryptGetEncode(publicKeyBase64) + " EQUALVERIFY CHECKSIG";
+        String lockingScript = "OP_DUP OP_HASH " + SHA256.encryptGetEncode(publicKeyBase64) + " OP_EQUALVERIFY OP_CHECKSIG OP_CHECKFINALRESULT";
         String unlockingScript = signature + " " + publicKeyBase64;
         Operator operator = new Operator(lockingScript, unlockingScript, message);
         // when
@@ -45,7 +50,84 @@ class OperatorTest {
 
         // then
         System.out.println("result = " + result);
+        assertTrue(result);
     }
+
+    @Test
+    @DisplayName("IF ELSE ENDIF 흐름 테스트")
+    void ifEndifTest() {
+        // given
+        String unlockingScript = "true";
+        String lockingScript = "OP_IF 5 OP_ELSE 10 OP_ENDIF OP_CHECKFINALRESULT";
+        // when
+        Operator operator = new Operator(lockingScript, unlockingScript, "message");
+        boolean validate = operator.validate();
+        System.out.println("validate = " + validate);
+        // then
+    }
+
+    @Test
+    @DisplayName("IF ELSE ENDIF 흐름 테스트 복잡한 버전")
+    void ifEndifTest2() {
+        // given
+        String unlockingScript = "true";
+        String lockingScript = "OP_IF 5 <sig> <sig2> <sig3> OP_ELSE 10 <pub> <pub2> OP_ENDIF OP_CHECKFINALRESULT";
+        // when
+        Operator operator = new Operator(lockingScript, unlockingScript, "message");
+        boolean validate = operator.validate();
+        System.out.println("validate = " + validate);
+        // then
+    }
+
+
+    @Test
+    @DisplayName("MULTISIG SCRIPT test 성공 사례 - M개의 유효 서명")
+    void testMultiSigWithValidSignatures() {
+        // given
+        int n = 3; // 총 공개 키 개수
+        int m = 2; // 필요한 서명 개수
+
+        String message = "Test multisig message";
+
+        // N개의 키 쌍 생성 (공개 키, 개인 키)
+        List<KeyPair> keyPairs = new ArrayList<>();
+
+        List<String> publicKeys = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            KeyPair keyPair = ECDSA.generateRandomKeyPair();
+            keyPairs.add(keyPair);
+            String publicKeyBase64 = Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded());
+            publicKeys.add(publicKeyBase64);
+        }
+
+        // M개의 서명 생성
+        List<String> signatures = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            KeyPair keyPair = keyPairs.get(i); // 첫 M개의 키를 사용하여 서명 생성
+            String signature = ECDSA.sign(message, keyPair.getPrivate());
+            signatures.add(signature);
+        }
+
+        // LOCKING SCRIPT 구성
+        // "M <pubKey1> <pubKey2> ... <pubKeyN> N OP_CHECKMULTISIG OP_CHECKFINALRESULT"
+        String lockingScript = m + " " + String.join(" ", publicKeys) + " " + n + " OP_CHECKMULTISIG OP_CHECKFINALRESULT";
+
+        // UNLOCKING SCRIPT 구성
+        // "<sig1> <sig2> ..."
+        String unlockingScript = String.join(" ", signatures);
+
+        // Operator 생성
+        Operator operator = new Operator(lockingScript, unlockingScript, message);
+
+        // when
+        boolean result = operator.validate();
+
+        // then
+        System.out.println("result = " + result);
+        assertTrue(result);
+    }
+
+
 
 
 
